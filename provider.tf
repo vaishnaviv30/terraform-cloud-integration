543c14c49b4eff5cf31e783747417078f3a5b17a
vaishnaviv30 <vaishnavivb3007@gmail.com>
2025-11-07 16:37:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -19,26 +19,6 @@ terraform {
 provider "azurerm" {
   # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
-}
-
-variable "client_id" {
-  type = string
-}
- 
-variable "client_secret" {
-  type = string
-}
- 
-variable "subscription_id" {
-  type = string
-}
- 
-variable "tenant_id" {
-  type = string
 }
 
 
