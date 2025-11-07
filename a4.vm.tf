7558a61ad4846f030b20d8724bd5fb2364a046ef
vaishnavi <vaishnavivb3007@gmail.com>
2025-11-07 06:21:30 +0000

    changed the instance type to production

--- a4.vm.tf
+++ a4.vm.tf
@@ -5,7 +5,7 @@ resource "azurerm_linux_virtual_machine" "web_vm" {
   location = azurerm_resource_group.rg-kyn.location
   #azurerm_resource_group.refrence_label.location
   resource_group_name = azurerm_resource_group.rg-kyn.name
-  size                = var.vm_instance_types["testing"] #refrence the instance size from instance-size.tf
+  size                = var.vm_instance_types["production"] #refrence the instance size from instance-size.tf
   admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.web_nic.id, #attach nic to vm
